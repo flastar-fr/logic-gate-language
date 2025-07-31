397e9d7d235ce471f6e627e03fd80c957db15cfd
flastar <hgbequet@gmail.com>
2025-07-31 13:57:31 +0200

    Add NOT gate as a default gate

--- src/language_processing/default_gates.hpp
+++ src/language_processing/default_gates.hpp
@@ -5,8 +5,10 @@
 
 const auto and_gate = Gate({"op1", "op2"}, {"out"}, 0b1000, true);
 const auto or_gate = Gate({"op1", "op2"}, {"out"}, 0b1110, true);
+const auto not_gate = Gate({"in"}, {"out"}, 0b01, true);
 
 const std::unordered_map<std::string, Gate> DEFAULT_GATES = {
     {"and", and_gate},
-    {"or", or_gate}
+    {"or", or_gate},
+    {"not", not_gate}
 };
